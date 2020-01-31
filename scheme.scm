9bb3de17567fe8d274b5c267625f2d7420f1370b
elijahprentice <prentee1213@gmail.com>
2020-01-31 17:28:44 -0500

    if element is a list, reverse that list (error)

--- scheme.scm
+++ scheme.scm
@@ -1,7 +1,10 @@
 (DEFINE (reverse-general L newList)
         (IF (NULL? L)
             newList
-            (reverse-general (CDR L) (CONS (CAR L) newList))
+            (IF (LIST? (CAR L))
+                (reverse-general (CDR L) (CONS (reverse-general(CAR L) (newList)) newList)))
+             (reverse-general (CDR L) (CONS (CAR L) newList))
+             )
          )
  )
 (reverse-general (READ) '())
